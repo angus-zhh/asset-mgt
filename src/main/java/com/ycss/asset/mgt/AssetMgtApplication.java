f990c317ab8e00fa8c9fe575379df464846db96b
zhhong <zhhong@estee.com>
2024-07-11 11:08:18 +0800

    项目初始化

---
+++ src/main/java/com/ycss/asset/mgt/AssetMgtApplication.java
@@ -0,0 +1,13 @@
+package com.ycss.asset.mgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetMgtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetMgtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ycss/asset/mgt/AssetMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycss.asset.mgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetMgtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
